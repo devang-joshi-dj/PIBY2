30ac0a798811da9831fb7d2a0b27da0403ace9f3
Devang Joshi <devangjoshi11@gmail.com>
2021-11-01 16:19:14 +0530

    Added Initial PlayArea

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Contents from './components/Contents/Contents';
+import PlayArea from './components/PlayArea/PlayArea';
 import Footer from './components/Footer/Footer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Header />
         <Contents />
+        <PlayArea />
         <Footer />
       </div>
     </>
---
+++ src/components/PlayArea/PlayArea.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './play-area.css';
+
+const PlayArea = () => {
+    return (
+        <>
+            <div className="play-area">
+
+            </div>
+        </>
+    );
+}
+
+export default PlayArea;
