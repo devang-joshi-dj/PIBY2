9c02d1b4fe26f461886f0de7ee5adfefd7d71232
Devang Joshi <devangjoshi11@gmail.com>
2021-11-01 22:41:32 +0530

    Fixed indexing Bug in ContentsList

--- src/components/Contents/Sub-Contents/ContentsList.js
+++ src/components/Contents/Sub-Contents/ContentsList.js
@@ -13,15 +13,16 @@ const ContentsList = (props) => {
             <div className="contents-list">
                 <ul>
                     {props.foundContents && props.foundContents.length > 0 ? (
-                        props.foundContents.map((user) => (
+                        props.foundContents.map((content) => (
                             <Link
+                                key={content.id}
                                 onClick={showPlayArea}
                                 to="play-area"
                                 smooth={true}
                                 duration={1000}
                             >
-                                <li key={user.id} className="content">
-                                    <span className="content-name">{user.name}</span>
+                                <li className="content">
+                                    <span className="content-name">{content.name}</span>
                                 </li>
                             </Link>
                         ))
