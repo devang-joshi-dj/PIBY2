4d6cd99ae20c95cab8f4b85f516dfb4a21cf551f
Devang Joshi <devangjoshi11@gmail.com>
2021-11-02 15:05:35 +0530

    Added Active Content Functionality

--- src/components/Contents/Sub-Contents/ContentsList.js
+++ src/components/Contents/Sub-Contents/ContentsList.js
@@ -6,23 +6,35 @@ const ContentsList = (props) => {
     const showPlayArea = () => {
         const getPlayArea = document.querySelector('.play-area');
         getPlayArea.style.display = 'block';
+        // var current = document.getElementsByClassName('active');
+        // current[0].className = current[0].className.replace(' active', '');
+        // this.className += ' active';
+    }
+
+
+    const setActiveContent = (index) => {
+        const contentName = document.querySelectorAll('.content');
+        for (var i = 0; i < contentName.length; i++) {
+            contentName[i].className = contentName[i].className.replace(" activeContent", "");
+        }
+        contentName[index - 1].className += ' activeContent';
     }
 
     return (
         <>
-            <div className="contents-list">
+            <div className='contents-list'>
                 <ul>
                     {props.foundContents && props.foundContents.length > 0 ? (
                         props.foundContents.map((content) => (
                             <Link
-                                key={content.id}
+                                key={content.index}
                                 onClick={showPlayArea}
-                                to="play-area"
+                                to='play-area'
                                 smooth={true}
                                 duration={1000}
                             >
-                                <li className="content">
-                                    <span className="content-name">{content.name}</span>
+                                <li className='content' onClick={() => setActiveContent(content.index)}>
+                                    <span className='content-name'>{content.name}</span>
                                 </li>
                             </Link>
                         ))
